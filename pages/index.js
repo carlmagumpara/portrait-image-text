fe98c8c5e26ccac91e5900c9534241ed006446d3
Carl Anthony Magumpara <carlmagumpara@MacBook-Pro.local>
2021-11-20 19:18:10 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,207 +1,149 @@
+import { useState } from 'react';
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
+  const [text, setText] = useState({
+    text: 'hotdog hotdog hotdog hotdog hotdog hotdog hotdog hotdog hotdog hotdog hotdog hotdog hotdog hotdog',
+    backgroundSize: 'cover',
+    backgroundPosition: 'top',
+    fontSize: 12,
+    lineHeight: 12,
+  });
+  const [image, setImage] = useState(null);
+
+  const onChange = event => {
+    if (event?.target?.files?.[0]) {
+      setImage(window.URL.createObjectURL(event?.target?.files?.[0]));
+    } else {
+      setText(prevState => ({
+        ...prevState,
+        [event.target.name]: event.target.value,
+      }));
+    }
+  };
+
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="wrapper">
+        <div className="sidebar">
+          <div>
+            <label className="text-label">
+              Photo
+            </label>
+            <label for="file-upload" className="custom-file-upload">
+              Select Photo
+              <input 
+                type="file" 
+                accept="image/*"
+                required 
+                capture 
+                onChange={onChange}
+                className="file-input"
+                id="file-upload"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="text-label">
+              Text
+            </label>
+            <textarea 
+              name="text"
+              rows="20" 
+              value={text.text} 
+              onChange={onChange}
+              className="text-input" 
+            />
+          </div>
+          <div>
+            <label className="text-label">
+              Background Size
+            </label>
+            <select 
+              name="backgroundSize"
+              onChange={onChange}
+              className="select-input"
+              value={text.backgroundSize} 
+            >
+            {['auto', 'length', 'cover', 'contain', 'initial', 'inherit'].map(item => (
+              <option value={item}>{item}</option>
+            ))}
+            </select>
+          </div>
+          <div>
+            <label className="text-label">
+              Font Size
+            </label>
+            <input
+              name="fontSize"
+              value={text.fontSize} 
+              onChange={onChange}
+              className="text-input" 
+            />
+          </div>
+          <div>
+            <label className="text-label">
+              Line Height
+            </label>
+            <input
+              name="lineHeight"
+              value={text.lineHeight} 
+              onChange={onChange}
+              className="text-input" 
+            />
+          </div>
+          <div>
+            <label className="text-label">
+              Background Position
+            </label>
+            <select 
+              name="backgroundPosition"
+              onChange={onChange}
+              className="select-input"
+              value={text.backgroundPosition} 
+            >
+            {[
+              'left top',
+              'left center',
+              'left bottom',
+              'right top',
+              'right center',
+              'right bottom',
+              'center top',
+              'center center',
+              'center bottom',
+            ].map(item => (
+              <option value={item}>{item}</option>
+            ))}
+            </select>
+          </div>
+          <p className="credits">Created by Carl</p>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
+        <div className="section">
+          <div className="text-wrapper">
+            {image && <p className="text default">{text.text.repeat(100)}</p>}
+          </div>
+        </div>
+      </div>
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .credits {
+          margin-top: 15px;
+          font-size: 12px;
         }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
+        .text {
+          text-align: justify;
+          font-size: ${text.fontSize}px;
+          line-height: ${text.lineHeight}px;
+          background: url(${image});
+          background-attachment: fixed;
+          background-position: ${text.backgroundPosition};
+          background-repeat: no-repeat;
+          background-size: ${text.backgroundSize};
+          color: transparent;
+          -webkit-text-fill-color: transparent;
+          background-clip: text;
+          -webkit-background-clip: text;
         }
       `}</style>
     </div>
